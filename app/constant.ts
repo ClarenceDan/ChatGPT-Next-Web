ec3c39b1c476bbb31b25243596ff133e9af27bf8
Clarence Dan <tangdan0854@gmail.com>
2023-11-06 17:49:52 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -21,7 +21,7 @@ export const UNAUTHORIZED_TEXT = `
 **提问：** 偶尔无法使用？\n 
 **回答：** 当前模型用量较高，到设置换个模型试试？\n\n
 **提问：** 所有模型均无法使用？\n 
-**回答：** 当日用户量太大资源耗尽，可加群提醒补充额度，或等待额度刷新\n 
+**回答：** 当日用户量太大资源耗尽，可等待额度刷新，或 **[获得官方账号](https://a01.aivesa.shop/)** \n 
 **推荐：** 推荐使用 **[Aivesa Pro](https://aivesa.cn/)**，高速稳定支持绘画 [👉点这里使用 Pro 版本](https://aivesa.cn/) \n\n 
 常见问题与使用说明点击查看👉 **[Aivesa产品简介](https://sourl.cn/uTB6WS)** \n\n
 欢迎来聊聊 🐧 [QQ频道](https://sourl.cn/XFbJKM) 或 🚀 [TG群组](https://t.me/+gJ-GQFE1GN40NGJl)\n\n 
